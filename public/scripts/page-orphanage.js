13b8f6bee68294862e1e970c13403efee2806f5c
Gilmara Pimentel <gilmarapq@hotmail.com>
2020-10-13 21:42:52 -0400

    refactor: refactor code on orphanagejs

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -1,82 +1,50 @@
 const options = {
-    dragging: false,
-    touchZoom: false,
-    doubleClickZoom: false,
-    scrollWheelZoom: false,
-    zoomControl: false
-    
-}
-
+  dragging: false,
+  touchZoom: false,
+  doubleClickZoom: false,
+  scrollWheelZoom: false,
+  zoomControl: false,
+};
 
 //create map
-const map = L.map('mapid', options ).setView([-27.2143428,-49.6388383], 15); // this is our map (setview has latitude, longitude, zoom)
+const map = L.map("mapid", options).setView([-27.2143428, -49.6388383], 15); // this is our map (setview has latitude, longitude, zoom)
 
-//create and add titeLayer 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
-.addTo(map)
+//create and add titeLayer
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
-
-// create ICON  - It is on the documentation how to do it. 
- const icon = L.icon({
-     iconUrl: "./public/images/map-marker.svg",
-     iconSize: [58, 68],
-     iconAnchor: [29, 68],
-     popupAnchor: [170, 2]
- })
-
-
-
+// create ICON  - It is on the documentation how to do it.
+const icon = L.icon({
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
 
 //create and ADD marker
-L
-.marker([-27.2143428,-49.6388383],{icon})
-.addTo(map)
-
-
-
-
+L.marker([-27.2143428, -49.6388383], { icon }).addTo(map);
 
 // Image Gallery
 
- function selectImage(event){
+function selectImage(event) {
+  //remove all Active classes from the buttons
 
-    const button = event.currentTarget // clicked button
+  const buttons = document.querySelectorAll(".images button");
+  buttons.forEach(removeActiveClass);
 
-    //remove all Active classes from the buttons
+  function removeActiveClass(button) {
+    button.classList.remove("active");
+  }
 
-    const buttons = document.querySelectorAll(".images button")
-    buttons.forEach(removeActiveClass)
+  const button = event.currentTarget; // clicked button
+  //console.log(button.children)   HTML collection with 1 position
+  // Select the clicked image
+  const image = button.children[0];
 
-        function removeActiveClass(button) {
-            button.classList.remove("active")
+  // Updated the container of image ( the bigger image)
+  const imageContainer = document.querySelector(".orphanage-details > img");
 
-        }
+  imageContainer.src = image.src;
 
-        // Select the clicked image
-        const image = button.children[0]
-        const imageContainer = document.querySelector(".orphanage-details > img")
-        imageContainer.src=image.src
-
-         // Add an Active Class to the current button
-        button.classList.add("active")
-    }
-
- 
-
-
-    
-
-
-    // Updated the container of image ( the bigger image)
-
-
-   
-
-
-
-
-
-   
-    
-
-
+  // Add an Active Class to the current button
+  button.classList.add("active");
+}
